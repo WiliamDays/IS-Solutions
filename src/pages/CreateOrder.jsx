b02eef3e74b87da0b76360650a57a063cd9febea
Wiliam Pereira Dias <80425917+WiliamDays@users.noreply.github.com>
2025-08-26 17:58:28 -0300

    Adicionando data e hora na realização do novo pedido

--- src/pages/CreateOrder.jsx
+++ src/pages/CreateOrder.jsx
@@ -1,17 +1,20 @@
-import React from "react";
 import Button from "../components/Button";
+import React, { useState, useEffect } from "react";
 
 function CreateOrder(){
-    
+    const [dataPedido, setDataDoPedido] = useState(null);
+
+    useEffect(() => {
+        setDataDoPedido(new Date()); 
+    }, []);
+
     return(
         <>            
             <h1>Novo pedido</h1>
             <div>
-
-
-                {/*Por conta dessa estrutura abaixo que não está exibindo no navegador (trabalhar ela)
-                    <p>{nomeDaEmpresa}</p>
-                    <p>{dataPedido}</p>
+                <p>Data do pedido: {dataPedido && dataPedido.toLocaleString()}</p>
+                
+                {/*Por conta dessa estrutura abaixo que não está exibindo no navegador (trabalhar ela)   
                     <p>{numeroPedido}</p>
                     <p>{cliente}</p>
                 */}
